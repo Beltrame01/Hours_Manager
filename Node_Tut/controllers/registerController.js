95d617c1214ee3c2d657dc22c54e6f94e28a792a
Beltrame01 <beltrame.vitor1@gmail.com>
2023-10-02 20:21:35 -0300

    feature 10

---
+++ Node_Tut/controllers/registerController.js
@@ -0,0 +1,15 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data}
+}
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req,res) => {
+    const { user,pwd } = req.body;
+    
+    if(!user || !pwd) return res.status(400).json({'message': 'Username and Password are required.'});
+
+    
+}
